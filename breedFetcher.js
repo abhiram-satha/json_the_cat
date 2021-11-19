9a74e2562edaf98edeaef8739b6a820c9c39ed03
Abhiram <abhiram.satha@gmail.com>
2021-11-19 02:47:46 +0000

    Added error message

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,8 +5,14 @@ let catApiKey = `https://api.thecatapi.com/v1/breeds/search?q=${catSearch}`;
 //console.log(catApiKey);
 
 request(catApiKey, (error, response, body) => {
+  
   //console.log('error', error);
   //console.log('statusCode:', response & response.statusCode);
   
-  console.log('body:', body);
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    return console.log('Please try again, this breed of cat does not exist.')
+  }
+  console.log(data['0']['description']);
+
 })
